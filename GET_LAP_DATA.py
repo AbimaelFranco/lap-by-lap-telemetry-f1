490eef485f29a88a5c20914d400ed82b8c0247fb
Abimael Franco <3006701680101@ingenieria.usac.edu.gt>
2025-07-04 20:57:41 -0600

    corrections in documentation

--- GET_LAP_DATA.py
+++ GET_LAP_DATA.py
@@ -1,5 +1,4 @@
 from data_fetcher.openf1_client import fetch_laps_data
-import pandas as pd
 from data_fetcher.utils import verify_data, save_data
 
 DRIVER_NUMBER = 81  # Lando Norris
@@ -7,7 +6,7 @@ SESSION_KEY = 9955  # Carrera en Spielberg
 
 
 def main():
-    print("Descargando telemetría de vueltas para Lando Norris...")
+    print("Downloading lap telemetry data for Lando Norris...")
     df = fetch_laps_data(DRIVER_NUMBER, SESSION_KEY)
 
     save_data(df, DRIVER_NUMBER, SESSION_KEY)
--- data_fetcher/openf1_client.py
+++ data_fetcher/openf1_client.py
@@ -5,7 +5,7 @@ API_BASE = "https://api.openf1.org/v1"
 HEADERS = {}
 
 def fetch_laps_data(driver_number: int, session_key: int) -> pd.DataFrame:
-    """Descarga la telemetría del piloto para una sesión dada."""
+    """Downloads the telemetry data for each lap of a driver in a given session."""
     url = f"{API_BASE}/laps"
     params = {"driver_number": driver_number, "session_key": session_key}
     r = requests.get(url, params=params, headers=HEADERS)
@@ -14,7 +14,7 @@ def fetch_laps_data(driver_number: int, session_key: int) -> pd.DataFrame:
     return df
 
 def fetch_car_data(driver_number: int, session_key: int) -> pd.DataFrame:
-    """Descarga los datos del coche para un piloto en una sesión dada."""
+    """Downloads the car data for a driver in a given session."""
     url = f"{API_BASE}/car_data"
     params = {"driver_number": driver_number, "session_key": session_key}
     r = requests.get(url, params=params, headers=HEADERS)
