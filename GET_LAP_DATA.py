bc1c623d9850c59b78f95aaf9dc4df70f946898c
Abimael Franco <3006701680101@ingenieria.usac.edu.gt>
2025-07-04 20:18:26 -0600

    corrections in get_lap_data.py

--- GET_LAP_DATA.py
+++ GET_LAP_DATA.py
@@ -1,19 +1,9 @@
-import requests
+from data_fetcher.openf1_client import fetch_laps_data
 import pandas as pd
 
-API_BASE = "https://api.openf1.org/v1"
-DRIVER_NUMBER = 4  # Lando Norris
+DRIVER_NUMBER = 81  # Lando Norris
 SESSION_KEY = 9955  # Carrera en Spielberg
-HEADERS = {}
 
-def fetch_laps_data(driver_number: int, session_key: int) -> pd.DataFrame:
-    """Descarga la telemetría del piloto para una sesión dada."""
-    url = f"{API_BASE}/laps"
-    params = {"driver_number": driver_number, "session_key": session_key}
-    r = requests.get(url, params=params, headers=HEADERS)
-    r.raise_for_status()
-    df = pd.DataFrame(r.json())
-    return df
 
 def main():
     print("Descargando telemetría de vueltas para Lando Norris...")
@@ -28,8 +18,8 @@ def main():
         pd.set_option('display.max_colwidth', None)
 
         print(df.head())  # Muestra filas específicas para evitar saturación
-        df.to_csv("norris_telemetria_laps_9955.csv", index=False)
-        df.to_excel("norris_telemetria_laps_9955.xlsx", index=False)
+        df.to_csv(f"{DRIVER_NUMBER}_telemetria_laps_{SESSION_KEY}.csv", index=False)
+        df.to_excel(f"{DRIVER_NUMBER}_telemetria_laps_{SESSION_KEY}.xlsx", index=False)
         print(f"Datos guardados en '{DRIVER_NUMBER}_telemetria_{SESSION_KEY}.csv' y en '{DRIVER_NUMBER}_telemetria_{SESSION_KEY}.xlsx'  ({len(df)} filas)")
 
 
