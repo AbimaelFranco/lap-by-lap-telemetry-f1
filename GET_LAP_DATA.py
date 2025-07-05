4ee3ab1af7bc180dd65542273af505e166b60f14
Abimael Franco <3006701680101@ingenieria.usac.edu.gt>
2025-07-04 20:53:29 -0600

    actualization to save data from utils

--- GET_LAP_DATA.py
+++ GET_LAP_DATA.py
@@ -1,5 +1,6 @@
 from data_fetcher.openf1_client import fetch_laps_data
 import pandas as pd
+from data_fetcher.utils import verify_data, save_data
 
 DRIVER_NUMBER = 81  # Lando Norris
 SESSION_KEY = 9955  # Carrera en Spielberg
@@ -9,19 +10,7 @@ def main():
     print("Descargando telemetría de vueltas para Lando Norris...")
     df = fetch_laps_data(DRIVER_NUMBER, SESSION_KEY)
 
-    if df.empty:
-        print("No se encontraron datos de telemetría.")
-    else:
-        pd.set_option('display.max_rows', None)
-        pd.set_option('display.max_columns', None)
-        pd.set_option('display.width', 0)
-        pd.set_option('display.max_colwidth', None)
-
-        print(df.head())  # Muestra filas específicas para evitar saturación
-        df.to_csv(f"{DRIVER_NUMBER}_telemetria_laps_{SESSION_KEY}.csv", index=False)
-        df.to_excel(f"{DRIVER_NUMBER}_telemetria_laps_{SESSION_KEY}.xlsx", index=False)
-        print(f"Datos guardados en '{DRIVER_NUMBER}_telemetria_{SESSION_KEY}.csv' y en '{DRIVER_NUMBER}_telemetria_{SESSION_KEY}.xlsx'  ({len(df)} filas)")
-
+    save_data(df, DRIVER_NUMBER, SESSION_KEY)
 
 if __name__ == "__main__":
     main()
--- data_fetcher/utils.py
+++ data_fetcher/utils.py
@@ -1,19 +1,34 @@
 import pandas as pd
 
-def verify_data(df: pd.DataFrame):
+def verify_data(df: pd.DataFrame) -> bool:
     """Verifies if the DataFrame is empty and shows useful info."""
     if df.empty:
         print("No telemetry data found.")
+        return False
     else:
         set_full_display()
         num_rows = len(df)
         num_cols = len(df.columns)
         column_list = ', '.join(df.columns)
 
-        print(f"DataFrame contains {num_rows} rows and {num_cols} columns.")
+        print(f"Telemetry contains {num_rows} rows and {num_cols} columns.")
         print(f"Got the columns: {column_list}")
+        return True
 
-def set_full_display():
+def save_data(df: pd.DataFrame, driver_number: int, session_key: int) -> None:
+    """Saves the DataFrame to CSV and Excel files."""
+    if verify_data(df):
+        csv_filename = f"{driver_number}_telemetry_laps_{session_key}.csv"
+        excel_filename = f"{driver_number}_telemetry_laps_{session_key}.xlsx"
+
+        df.to_csv(csv_filename, index=False)
+        df.to_excel(excel_filename, index=False)
+        print(f"Data saved to '{csv_filename}' and '{excel_filename}'")
+    else:
+        print("No data to save.")
+        
+
+def set_full_display() -> None:
     """Configures pandas to display all rows and columns without truncation."""
     pd.set_option('display.max_rows', None)
     pd.set_option('display.max_columns', None)
