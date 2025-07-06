5ee081f8ef24ec6116e5cd7cad8be6eaa7fe655d
Abimael Franco <3006701680101@ingenieria.usac.edu.gt>
2025-07-05 18:31:47 -0600

    need correct date in laps and drivers saved

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
-from data_fetcher.openf1_client import fetch_laps_data, fetch_car_data
-from data_fetcher.utils import save_data, verify_data
+from data_fetcher.openf1_client import fetch_laps_data, fetch_car_data, fetch_drivers_data
+from data_fetcher.utils import save_data, verify_data, split_datetime_column
 
 DRIVER_NUMBER = 4  # Lando Norris
 SESSION_KEY = 9955  # Carrera en Spielberg
@@ -9,9 +9,11 @@ def main():
     print("Downloading lap telemetry data for Lando Norris...")
     laps_df = fetch_laps_data(DRIVER_NUMBER, SESSION_KEY)
     car_df = fetch_car_data(DRIVER_NUMBER, SESSION_KEY)
+    drivers_df = fetch_drivers_data(SESSION_KEY)
 
-    save_data(laps_df, DRIVER_NUMBER, SESSION_KEY, "laps")
-    save_data(car_df, DRIVER_NUMBER, SESSION_KEY, "car_data")
+    save_data(laps_df, SESSION_KEY, "laps", DRIVER_NUMBER)
+    save_data(car_df, SESSION_KEY, "car_data", DRIVER_NUMBER)
+    save_data(drivers_df, SESSION_KEY, "drivers")
 
 if __name__ == "__main__":
     main()
