66726da06156995fb98a59d2c92048c9446346db
Abimael Franco <3006701680101@ingenieria.usac.edu.gt>
2025-07-07 21:03:20 -0600

    corrections params order in fetch functions

--- src/data_fetcher/openf1_client.py
+++ src/data_fetcher/openf1_client.py
@@ -5,7 +5,7 @@ from typing import Optional
 API_BASE = "https://api.openf1.org/v1"
 HEADERS = {}
 
-def fetch_laps_data(driver_number: int, session_key: int) -> pd.DataFrame:
+def fetch_laps_data(session_key: int, driver_number: int) -> pd.DataFrame:
     """Downloads the telemetry data for each lap of a driver in a given session."""
     url = f"{API_BASE}/laps"
     params = {"driver_number": driver_number, "session_key": session_key}
@@ -14,7 +14,7 @@ def fetch_laps_data(driver_number: int, session_key: int) -> pd.DataFrame:
     df = pd.DataFrame(r.json())
     return df
 
-def fetch_car_data(driver_number: int, session_key: int) -> pd.DataFrame:
+def fetch_car_data(session_key: int, driver_number: int) -> pd.DataFrame:
     """Downloads the car data for a driver in a given session."""
     url = f"{API_BASE}/car_data"
     params = {"driver_number": driver_number, "session_key": session_key}
@@ -31,13 +31,13 @@ def fetch_drivers_data(session_key: int, driver_number: Optional[int] = None) ->
         params = {"session_key": session_key, "driver_number": driver_number}
     else:
         params = {"session_key": session_key}
-        
+
     r = requests.get(url, params=params, headers=HEADERS)
     r.raise_for_status()
     df = pd.DataFrame(r.json())
     return df
 
-def fetch_intervals_data(driver_number: int, session_key: int) -> pd.DataFrame:
+def fetch_intervals_data(session_key: int, driver_number: int) -> pd.DataFrame:
     """Downloads the intervals data in a given session."""
     url = f"{API_BASE}/intervals"
     params = {"driver_number": driver_number, "session_key": session_key}
